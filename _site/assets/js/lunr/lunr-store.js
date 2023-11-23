9003c70b6109390538fdff3d7a63c5a5efc1145f
Daniel <daniel.lazaro.iglesias@gmail.com>
2023-11-23 14:25:14 +0100

    Ignore

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,23 +3,29 @@ var store = [{
         "excerpt":"Astronautas, hombres lobo, cyborgs, vampiros, hackers, telépatas, asesinos a sueldo, asesinos por afición, asesinos por otros motivos… ¡Lee sus desventuras en las páginas de esta trepidante colección de relatos! Un grupo de científicos orbita un misterioso planeta cuya superfície está plagada de misteriosas estructuras de roca. Un hombre busca al...","categories": [],
         "tags": [],
         "url": "/elobservador/",
-        "teaser": "/assets/img/elobservador-cover.png"
+        "teaser": "/assets/img/elobservador-cover.avif"
       },{
         "title": "La Distorsión",
         "excerpt":"Una aventura de ciencia ficción cargada de acción, en un mundo postapocalíptico lleno de misterios y plagado de criaturas lovecraftianas. Cuando el búnker de la Torre, el refugio subterráneo en el que vive, pierde la comunicación con sus vecinos, Fran parte al exterior con un pequeño equipo de exploradores a...","categories": [],
         "tags": [],
         "url": "/ladistorsion/",
-        "teaser": "/assets/img/ladistorsion-cover.png"
+        "teaser": "/assets/img/ladistorsion-cover.avif"
       },{
-        "title": "_La Distorsión_ disponible en Amazon en ebook y en tapa blanda",
-        "excerpt":"La Distorsión está disponible en Amazon tanto en versión de ebook como en tapa blanda. Además, la versión digital se puede leer de forma gratuita si eres miembro de Kindle Unlimited.   Puedes leer las primeras páginas a continuación:    ","categories": ["libros","novedades"],
+        "title": "_\"La Distorsión\"_ disponible en Amazon en ebook y en tapa blanda",
+        "excerpt":"“La Distorsión” está disponible en Amazon tanto en versión de ebook como en tapa blanda. Además, la versión digital se puede leer de forma gratuita si eres miembro de Kindle Unlimited.   Puedes leer las primeras páginas a continuación:    ","categories": ["libros","novedades"],
         "tags": [],
         "url": "/libros/novedades/2023/11/16/ladistorsion-disponible.html",
-        "teaser": "/assets/img/ladistorsion-cover.png"
+        "teaser": "/assets/img/ladistorsion-cover.avif"
       },{
-        "title": "_El observador y otros relatos_ ya a la venta en Amazon y Lektu",
-        "excerpt":"La colección de relatos cortos El observador y otros relatos está a la venta tanto en Amazon como en Lektu desde la semana pasada. En Amazon puedes adquirir tanto la versión en ebook como la versión en tapa blanda, mientras que en Lektu está disponible la versión digital en varios...","categories": ["libros","novedades"],
+        "title": "_\"El observador y otros relatos\"_ ya a la venta en Amazon y Lektu",
+        "excerpt":"La colección de relatos cortos “El observador y otros relatos” está a la venta tanto en Amazon como en Lektu desde la semana pasada. En Amazon puedes adquirir tanto la versión en ebook como la versión en tapa blanda, mientras que en Lektu está disponible la versión digital en varios...","categories": ["libros","novedades"],
         "tags": [],
         "url": "/libros/novedades/2023/11/20/elobservador-disponible.html",
-        "teaser": "/assets/img/elobservador-cover.png"
+        "teaser": "/assets/img/elobservador-cover.avif"
+      },{
+        "title": "Ya disponible en Google Play Books la colección _\"El observador y otros relatos\"_ ",
+        "excerpt":"Desde hoy, la colección de relatos cortos “El observador y otros relatos” está disponible en Google Play Books de forma gratuita. Solo necesitas entrar en este enlace y pulsar el botón Leer para empezar. ¡Espero que lo disfrutes! ¡Y, si te animas, no te olvides de dejar una reseña! :smile:...","categories": ["libros","novedades"],
+        "tags": [],
+        "url": "/libros/novedades/2023/11/23/elobservador-disponible-google.html",
+        "teaser": "/assets/img/elobservador-cover.avif"
       },]
